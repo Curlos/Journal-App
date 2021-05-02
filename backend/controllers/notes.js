183bc5f1fba034a4b9f4d1e529156057593cde6c
Carlos Martinez <curlosmart@gmail.com>
2021-05-02 18:38:06 -0400

    note routes created

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -6,5 +6,38 @@ notesRouter.get("/", async(request, response) => {
     response.json(notes.map((note) => note.toJSON()));
 });
 
+notesRouter.post("/", async (request, response, next) => {
+    const reqBody = request.body;
+
+    const note = new Note({
+        title: reqBody.title,
+        body: reqBody.body,
+    })
+
+    const savedNote = await note.save();
+    return response.json(savedNote);
+})
+
+notesRouter.put("/:id", (request, response, next) => {
+    const reqBody = request.body;
+
+    const note = {
+        title: reqBody.title,
+        body: reqBody.body
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then((updatedNote) => {
+            response.json(updatedNote);
+        })
+        .catch((error) => next(error));
+})
+
+notesRouter.delete("/:id", async (request, response) => {
+    console.log('deleting note...');
+    await Note.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+})
+
 
 module.exports = notesRouter;
--- backend/models/note.js
+++ backend/models/note.js
@@ -5,12 +5,4 @@ const noteSchema = new mongoose.Schema({
     body: String,
 });
 
-noteSchema.set("toJSON", {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    },
-});
-
 module.exports = mongoose.model("Note", noteSchema);
